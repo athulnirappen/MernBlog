a92e11628e64b0670b3355cd4591358b839af1bc
Athul Thomas <athulthomas001798@gmail.com>
2024-03-22 12:19:25 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
 
 app.listen(5000, () => {
-    console.log('server is running in 3000');
-})
+  console.log("server is running in 5000");
+});
